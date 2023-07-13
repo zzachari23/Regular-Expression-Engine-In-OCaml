ec2ddb8971b9fcbcc0a963f27e2f35a2d1f3b39d
Zachariah Zachariah <zachgit1999@gmail.com>
2023-07-13 19:27:31 -0400

    Add files via upload

---
+++ nfa.ml
@@ -0,0 +1,206 @@
+open List
+open Sets
+
+(*********)
+(* Types *)
+(*********)
+
+type ('q, 's) transition = 'q * 's option * 'q
+
+type ('q, 's) nfa_t = {
+  sigma: 's list;
+  qs: 'q list;
+  q0: 'q;
+  fs: 'q list;
+  delta: ('q, 's) transition list;
+}
+
+(***********)
+(* Utility *)
+(***********)
+
+(* explode converts a string to a character list *)
+let explode (s: string) : char list =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l)
+  in
+  exp (String.length s - 1) []
+
+(****************)
+(* Part 1: NFAs *)
+(****************)
+
+(* Helper for move *)
+let rec helper_checkDelta delta head option = 
+  match delta with 
+  |[] -> [] 
+  |h::t -> match h with 
+    |(a, b, c) -> if a = head && b = option then [c] @ helper_checkDelta t head option else helper_checkDelta t head option 
+;;
+
+(* Helper for move *)
+let rec helper_iState nfa list option = 
+  match list with 
+  |[] -> []
+  |h::t -> union (helper_checkDelta nfa.delta h option) (helper_iState nfa t option)
+;;
+
+let move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list =
+  helper_iState nfa qs s
+;;
+
+(* Helper for eclosure *)
+let rec helper_eclosure nfa iState list= 
+  match iState with 
+  | [] -> []
+  | h::t -> if (elem h list) != true then let newlist = helper_checkDelta nfa.delta h None 
+        in 
+        union [h] (helper_eclosure nfa (union t newlist) (union list [h]))
+      else helper_eclosure nfa t list
+;;
+
+let e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
+  helper_eclosure nfa qs []
+;;
+
+
+(*******************************)
+(* Part 2: Subset Construction *)
+(*******************************)
+
+(* Helper for new_states *)
+let rec helper_states nfa sigma qs = 
+  match sigma with 
+  |[] -> [] 
+  |h::t -> [e_closure nfa (move nfa qs (Some h))] @ helper_states nfa t qs 
+;;
+  
+(* Helper for new_trans *)
+let rec helper_trans nfa sigma qs = 
+  match sigma with 
+  |[] -> [] 
+  |h::t -> (qs, Some h, e_closure nfa (move nfa qs (Some h))) :: helper_trans nfa t qs 
+;;
+
+(* Helper for new_finals*)
+let rec helper_finals fs qs list = 
+  match fs with 
+  |[] -> []
+  |h::t -> if elem h qs = true then [list] else helper_finals t qs list
+;;
+
+
+let new_states (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
+  helper_states nfa (nfa.sigma) qs
+;;
+
+let new_trans (nfa: ('q,'s) nfa_t) (qs: 'q list) : ('q list, 's) transition list =
+  helper_trans nfa nfa.sigma qs
+;;
+
+
+let new_finals (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
+  let list = qs in 
+  helper_finals (nfa.fs) qs list 
+;;
+
+(***** HELPERS FOR NFA_TO_DFA *****)
+
+(* Get's final state *)
+let rec helper_dfa_final nfa r = 
+  match r with 
+  |[] -> [] 
+  |h::t -> new_finals nfa h @ helper_dfa_final nfa t 
+  
+;;
+
+(* Get's qs states *)
+let rec helper_dfa_states nfa dfa r =
+  match r with 
+  |[] -> []
+  |h::t -> if elem h dfa.qs = false then remove [] (helper_states nfa nfa.sigma h) @ helper_dfa_states nfa dfa t else helper_dfa_states nfa dfa t
+;; 
+
+(* Get's the transitions *)
+let rec helper_dfa_transitions nfa r = 
+  match r with 
+  |[] -> []
+  |h::t -> new_trans nfa h @ helper_dfa_transitions nfa t 
+;;
+
+  
+let rec nfa_to_dfa_step (nfa: ('q,'s) nfa_t) (dfa: ('q list, 's) nfa_t)
+    (work: 'q list list) : ('q list, 's) nfa_t =
+  let work = union work (helper_dfa_states nfa dfa work) in 
+  let work = remove [] (minus work dfa.qs) in 
+  let transitions = helper_dfa_transitions nfa work in 
+  match work with 
+  |[] -> dfa
+  |h::t -> 
+      match dfa with 
+      | { sigma = a
+        ; qs = b 
+        ; q0 = c
+        ; fs = d 
+        ; delta = e } -> let newdfa = { sigma = a
+                                      ; qs = remove [] (union b (h :: b))
+                                      ; q0 = c
+                                      ; fs = d 
+                                      ; delta = union e transitions }
+          in 
+          nfa_to_dfa_step nfa newdfa t 
+;;
+  ;;
+let nfa_to_dfa (nfa: ('q,'s) nfa_t) : ('q list, 's) nfa_t =
+  let q0state = e_closure nfa [nfa.q0]in 
+  let wrk =[q0state] in 
+  let dfa = 
+    { sigma = nfa.sigma
+    ; qs = []
+    ; q0 = q0state
+    ; fs = []
+    ; delta = [] } in 
+  let finaldfa = nfa_to_dfa_step nfa dfa wrk in
+  match finaldfa with 
+    { sigma = a
+    ; qs = b 
+    ; q0 = c
+    ; fs = d 
+    ; delta = e } -> let newdfa = { sigma = a
+                                  ; qs =  b
+                                  ; q0 = c
+                                  ; fs = helper_dfa_final nfa b
+                                  ; delta = e } in newdfa
+;;
+
+(* Helper for accept *)
+let rec helper_accept2 fs finalposition =
+  match fs with 
+  |[] -> false
+  |h::t -> if elem h finalposition then true else helper_accept2 t finalposition
+;;
+  
+(* Helper for accept *)
+let rec helper_accept nfa istate charlist = 
+  match charlist with 
+  | [] -> istate
+  | h::t -> helper_accept nfa (move nfa (e_closure nfa istate) (Some h)) t
+;;
+
+let accept (nfa: ('q,char) nfa_t) (s: string) : bool =
+  let charlist = explode s in 
+  let finalposition = helper_accept nfa [nfa.q0] charlist  in 
+  if helper_accept2 nfa.fs (e_closure nfa finalposition) = true then true else false
+;;
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ regexp.ml
@@ -0,0 +1,196 @@
+open List
+open Nfa
+open Sets
+
+(*********)
+(* Types *)
+(*********)
+
+type regexp_t =
+  | Empty_String
+  | Char of char
+  | Union of regexp_t * regexp_t
+  | Concat of regexp_t * regexp_t
+  | Star of regexp_t
+
+(***********)
+(* Utility *)
+(***********)
+
+let fresh =
+  let cntr = ref 0 in
+  fun () ->
+    cntr := !cntr + 1 ;
+    !cntr
+
+    
+(*******************************)
+(* Part 3: Regular Expressions *)
+(*******************************)
+
+let rec regexp_to_nfa (regexp: regexp_t) : (int, char) nfa_t =
+  match regexp with 
+  |Empty_String -> 
+      let state = fresh() in 
+      let nfa = { sigma = []
+                ; qs = [state]
+                ; q0 = state
+                ; fs = [state]
+                ; delta = [(state, None, state)] } in nfa 
+        
+  |Char (c) -> 
+      let sState = fresh() in 
+      let fState = fresh() in 
+      let nfa = { sigma = [c]
+                ; qs = [sState;fState]
+                ; q0 = sState
+                ; fs = [fState]
+                ; delta = [(sState, Some c, fState)] } in nfa 
+        
+  |Union (r1, r2) ->
+      let a = regexp_to_nfa r1 in 
+      let b = regexp_to_nfa r2 in 
+      let s0 = fresh() in 
+      let s1 = fresh() in 
+      let nfa = { sigma = union (a.sigma) (b.sigma)
+                ; qs = union (union a.qs b.qs) ([s0;s1])
+                ; q0 = s0
+                ; fs = [s1]
+                ; delta = union (union a.delta b.delta) ([(s0, None, a.q0); (s0, None, b.q0); (List.hd a.fs, None, s1); (List.hd b.fs, None, s1)]) } in nfa 
+      
+      
+  |Concat (r1, r2) -> 
+      let a = regexp_to_nfa r1 in 
+      let b = regexp_to_nfa r2 in 
+      let nfa = { sigma = union (a.sigma) (b.sigma) 
+                ; qs = union (a.qs) (b.qs)
+                ; q0 = a.q0
+                ; fs = b.fs
+                ; delta = union (union (a.delta) (b.delta)) ([(List.hd a.fs, None, b.q0)])} in nfa 
+        
+  |Star (r) -> 
+      let a = regexp_to_nfa r in 
+      let s0 = fresh() in 
+      let s1 = fresh() in 
+      let nfa = { sigma = a.sigma 
+                ; qs = union (a.qs) ([s0;s1])
+                ; q0 = s0
+                ; fs = [s1]
+                ; delta = union (a.delta) ([(List.hd a.fs, None, s1); (s0, None, a.q0); (s0, None, s1); (s1, None, s0)]) } in nfa
+      
+;;
+    
+
+(*****************************************************************)
+(* Below this point is parser code that YOU DO NOT NEED TO TOUCH *)
+(*****************************************************************)
+
+exception IllegalExpression of string
+
+(* Scanner *)
+type token =
+  | Tok_Char of char
+  | Tok_Epsilon
+  | Tok_Union
+  | Tok_Star
+  | Tok_LParen
+  | Tok_RParen
+  | Tok_END
+
+let tokenize str =
+  let re_var = Str.regexp "[a-z]" in
+  let re_epsilon = Str.regexp "E" in
+  let re_union = Str.regexp "|" in
+  let re_star = Str.regexp "*" in
+  let re_lparen = Str.regexp "(" in
+  let re_rparen = Str.regexp ")" in
+  let rec tok pos s =
+    if pos >= String.length s then [Tok_END]
+    else if Str.string_match re_var s pos then
+      let token = Str.matched_string s in
+      Tok_Char token.[0] :: tok (pos + 1) s
+    else if Str.string_match re_epsilon s pos then
+      Tok_Epsilon :: tok (pos + 1) s
+    else if Str.string_match re_union s pos then Tok_Union :: tok (pos + 1) s
+    else if Str.string_match re_star s pos then Tok_Star :: tok (pos + 1) s
+    else if Str.string_match re_lparen s pos then Tok_LParen :: tok (pos + 1) s
+    else if Str.string_match re_rparen s pos then Tok_RParen :: tok (pos + 1) s
+    else raise (IllegalExpression ("tokenize: " ^ s))
+  in
+  tok 0 str
+
+let tok_to_str t =
+  match t with
+  | Tok_Char v -> Char.escaped v
+  | Tok_Epsilon -> "E"
+  | Tok_Union -> "|"
+  | Tok_Star -> "*"
+  | Tok_LParen -> "("
+  | Tok_RParen -> ")"
+  | Tok_END -> "END"
+
+(*
+   S -> A Tok_Union S | A
+   A -> B A | B
+   B -> C Tok_Star | C
+   C -> Tok_Char | Tok_Epsilon | Tok_LParen S Tok_RParen
+
+   FIRST(S) = Tok_Char | Tok_Epsilon | Tok_LParen
+   FIRST(A) = Tok_Char | Tok_Epsilon | Tok_LParen
+   FIRST(B) = Tok_Char | Tok_Epsilon | Tok_LParen
+   FIRST(C) = Tok_Char | Tok_Epsilon | Tok_LParen
+ *)
+
+let parse_regexp (l : token list) =
+  let lookahead tok_list =
+    match tok_list with
+    | [] -> raise (IllegalExpression "lookahead")
+    | h :: t -> (h, t)
+  in
+  let rec parse_S l =
+    let a1, l1 = parse_A l in
+    let t, n = lookahead l1 in
+    match t with
+    | Tok_Union ->
+        let a2, l2 = parse_S n in
+        (Union (a1, a2), l2)
+    | _ -> (a1, l1)
+  and parse_A l =
+    let a1, l1 = parse_B l in
+    let t, n = lookahead l1 in
+    match t with
+    | Tok_Char c ->
+        let a2, l2 = parse_A l1 in
+        (Concat (a1, a2), l2)
+    | Tok_Epsilon ->
+        let a2, l2 = parse_A l1 in
+        (Concat (a1, a2), l2)
+    | Tok_LParen ->
+        let a2, l2 = parse_A l1 in
+        (Concat (a1, a2), l2)
+    | _ -> (a1, l1)
+  and parse_B l =
+    let a1, l1 = parse_C l in
+    let t, n = lookahead l1 in
+    match t with Tok_Star -> (Star a1, n) | _ -> (a1, l1)
+  and parse_C l =
+    let t, n = lookahead l in
+    match t with
+    | Tok_Char c -> (Char c, n)
+    | Tok_Epsilon -> (Empty_String, n)
+    | Tok_LParen ->
+        let a1, l1 = parse_S n in
+        let t2, n2 = lookahead l1 in
+        if t2 = Tok_RParen then (a1, n2)
+        else raise (IllegalExpression "parse_C 1")
+    | _ -> raise (IllegalExpression "parse_C 2")
+  in
+  let rxp, toks = parse_S l in
+  match toks with
+  | [Tok_END] -> rxp
+  | _ -> raise (IllegalExpression "parse didn't consume all tokens")
+
+
+let string_to_regexp str = parse_regexp @@ tokenize str
+
+let string_to_nfa str = regexp_to_nfa @@ string_to_regexp str
